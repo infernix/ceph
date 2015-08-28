3e3a25a643ab58091f6f3415b8077419b3cf4494
Casey Bodley <cbodley@redhat.com>
2015-08-28 10:32:40 -0400

    blkin: add traces to FileStore/Journal
    Signed-off-by: Casey Bodley <cbodley@redhat.com>

--- src/os/filestore/FileStore.h
+++ src/os/filestore/FileStore.h
@@ -34,6 +34,7 @@ using namespace std;
 #include "common/Timer.h"
 #include "common/WorkQueue.h"
 #include "common/perf_counters.h"
+#include "common/zipkin_trace.h"
 
 #include "common/Mutex.h"
 #include "HashIndex.h"
@@ -222,6 +223,7 @@ private:
     Context *onreadable, *onreadable_sync;
     uint64_t ops, bytes;
     TrackedOpRef osd_op;
+    ZTracer::Trace trace;
   };
   class OpSequencer : public Sequencer_impl {
     Mutex qlock; // to protect q, for benefit of flush (peek/dequeue also protected by lock)
@@ -296,6 +298,7 @@ private:
     void queue(Op *o) {
       Mutex::Locker l(qlock);
       q.push_back(o);
+      o->trace.keyval("queue depth", q.size());
     }
     Op *peek_queue() {
       Mutex::Locker l(qlock);
@@ -424,6 +427,8 @@ private:
   void new_journal();
 
   PerfCounters *logger;
+
+  ZTracer::Endpoint trace_endpoint;
 
 public:
   int lfn_find(const ghobject_t& oid, const Index& index,
